d8ab2c8e97a5d1d910d3cec8a0707c40b8bed603
Jediel <jediel.dev@gmail.com>
2026-04-29 21:18:39 -0300

    Removido o consumidor do Rabbitmq

--- src/FiapCloudGames.Queue/Configurations/QueueConfig.cs
+++ src/FiapCloudGames.Queue/Configurations/QueueConfig.cs
@@ -1,5 +1,4 @@
 using FiapCloudGames.Queue.Configurations.MassTransit;
-using FiapCloudGames.Queue.Configurations.Rabbitmq;
 using FiapCloudGames.Queue.Configurations.Sqs;
 using FiapCloudGames.Queue.Publishers;
 using Microsoft.Extensions.Configuration;
@@ -15,11 +14,6 @@ public static class QueueConfig
 
         services.AddScoped<IPaymentProcessedPublisher, PaymentProcessedPublisher>();
 
-        // RabbitMQ
-        services.Configure<RabbitmqSettings>(configuration.GetSection(nameof(RabbitmqSettings)));
-        services.RegisterRabbitmqStartup();
-
-        // AWS SQS
         services.Configure<SqsSettings>(configuration.GetSection(nameof(SqsSettings)));
         services.RegisterSqsStartup();
 
